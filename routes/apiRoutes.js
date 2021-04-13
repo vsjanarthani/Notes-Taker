f79958dcdd07a9f4932580d979bbee0f11f716b1
jana <vs.janarthani@gmail.com>
2021-04-12 22:33:20 -0700

    added api and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,34 @@
+
+const router = require('express').Router();
+const { notes } = require ('../data/db.json');
+const uuid = require('uuid');
+const { validateNotes, addNote, deleteNote } = require ('../lib/notes');
+
+router.get('/notes', (req, res) => res.json(notes));
+
+router.post('/notes', (req, res) => {
+    const newNote = {
+        id: uuid.v4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    if (!validateNotes(newNote)) {
+        return res.status(400).send("Please enter a title and text in the correct format");
+    } else {
+        addNote(newNote, notes);
+        res.json(notes);
+    }   
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const exists = notes.some(notes => notes.id === req.params.id);
+    if (exists) {
+        return res.json(deleteNote(req.params.id, notes));
+    } else {
+        res.status(400).send(`No notes found with the id ${req.params.id}`)
+    }
+
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join (__dirname, 'public')));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
