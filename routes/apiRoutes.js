3b18ef85c880f6d00363ab3dd1af987bf3b7af8c
jana <vs.janarthani@gmail.com>
2021-04-15 21:13:02 -0700

    trying to figure bug

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,10 @@
 const router = require('express').Router();
 let { notes } = require ('../data/db.json');
 const uuid = require('uuid');
-const { validateNotes, addNote } = require ('../lib/notes');
-const fs = require('fs');
-const path = require('path');
+const { validateNotes, addNote, deleteNote } = require ('../lib/notes');
 
 router.get('/notes', (req, res) => {
+    console.log(notes);
     res.json(notes);
     
 }); 
@@ -27,18 +26,13 @@ router.post('/notes', (req, res) => {
 });
 
 router.delete('/notes/:id', (req, res) => {
+    console.log("delete router got hit");
     const exists = notes.some(notes => notes.id === req.params.id);
     
     if (exists) {
-        notes = notes.filter(note => note.id !== req.params.id);
-        fs.writeFileSync(
-            path.join(__dirname, '../data/db.json'),
-            JSON.stringify({ notes }, null, 2)
-          );
-        console.log(notes);
-        res.json(notes);
+        return res.json(deleteNote(req.params.id, notes));
     } else {
-        res.status(400).send(`No notes found with the id ${req.params.id}`);
+        res.status(400).send(`No notes found with the id ${req.params.id}`)
     }
 
 });
